5af9e7beecadf886dd0fc50d712a58912445f7d6
Hourn Sovannara <hournsovannara18@kit.edu.kh>
2020-02-05 15:46:24 +0700

    Refactor Complete

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,7 +1,235 @@
 const express = require('express')
-const router = express.Router();
+const router = express.Router()
+const User = require("../models/user")
+const uID = require("../utilities/UniqueCode")
+const verify = require("../utilities/verifyToken")
+const Streaming = require('../models/streaming')
+
+//Get user Info
+router.get("/user", verify , async (req, res) => {
+    const email = req.user.email
+    const user = await User.findOne({email})
+    const {name,isStreaming} = user
+    const _user = {
+        email,
+        name,
+        isStreaming,
+        role:req.user.role
+    }
+    return res.json(_user)
+})
+
+// Start stream
+router.post("/startStream", verify, async (req, res) => {
+    const owner = req.user.email
+    const ownerName = req.user.name
+    const {streamTitle,description,isPrivate,password} = req.body
+    try{
+        var streamCode = null
+        var isNotUnique = null
+
+        do{
+            streamCode = uID(12)
+            isNotUnique = await Streaming.findOne({streamCode})
+        }while(isNotUnique)
+
+        const newStream = new Streaming({
+            streamCode,
+            streamTitle,
+            description,
+            isPrivate,
+            password,
+            owner,
+            ownerName
+        })
+        const savedStream = await newStream.save()
+        await User.updateOne({email:owner},{isStreaming : true})
+        // await axios.post('http://10.10.15.11:4000/createRoom',{roomName:streamTitle,roomId:streamCode}).catch(er => console.log(er))
+        return res.json({streamCode : savedStream.streamCode,streamTitle : savedStream.streamTitle, Description : savedStream.Description})
+    }catch (err){
+        console.log(err)
+        return res.json(err)
+    }
+
+})
+
+// Device start stream
+router.post('/deviceStartStream',verify,async(req,res)=>{
+    const deviceEmail = req.user.email
+    const {streamTitle,description,isPrivate,password,streamBy} = req.body
+    const _U = await User.findOne({email:streamBy})
+    try{
+        var streamCode = null
+        var isNotUnique = null
+        do{
+            streamCode = uID(12)
+            isNotUnique = await Streaming.findOne({streamCode})
+        }while(isNotUnique)
+
+        const newStream = new Streaming({
+            streamCode,
+            streamTitle,
+            description,
+            isPrivate,
+            password,
+            owner:streamBy,
+            ownerName:_U.name,
+            streamFrom:deviceEmail
+        })
+        const savedStream = await newStream.save()
+        await User.updateOne({email:streamBy},{currentStream:streamCode})
+        await User.updateOne({email:streamBy},{isStreaming : true})
+        await axios.post('http://10.10.15.11:4000/createRoom',{roomName:streamTitle,roomId:streamCode}).catch(er => console.log(er))
+        axios.post('http://10.10.15.11:3001/devices/redirect',{streamBy,streamCode}).catch((er)=> console.log(er))
+        return res.json({streamCode : savedStream.streamCode,streamTitle : savedStream.streamTitle, Description : savedStream.Description})
 
 
+    }catch (err){
+        console.log(err)
+        return res.json(err)
+    }
+})
 
+// Join Stream
+router.post("/joinStream", verify, async(req,res) => {
+    const email = req.user.email
+    const name = req.user.name
+    const streamCode = req.body.streamCode
+    const password = req.body.pwd
+    const domain = 'meet.jit.si';
+    
+    try{
+        //Get stream info
+        const theStream = await Streaming.findOne({streamCode});
+        // Check Stream status
+        if (!theStream.isActive){
+            return res.json({message : "Stream is not currently available", errCode : "ST-001"})
+        }
+        // Check Stream privacy
+        if (!theStream.isPrivate){
+            if (!password.equals("") && password.equals(null)){
+                if(!theStream.password.equals(password)){
+                    return res.json({message : "Incorrect password", errCode : "ST-002"})
+                }
+            }else{
+                return res.json({message : "Password is required", errCode : "ST-003"})
+            }     
+        }
+
+        // Check ownership
+        if ((theStream.owner === email && theStream.streamFrom == 'none') || (theStream.streamFrom === email)){ // Owner
+            // For Streamer/Lecturer
+            const interfaceConfigLecturer = {
+                TOOLBAR_BUTTONS: [
+                    'microphone', 'camera', 'closedcaptions', 'desktop', 'fullscreen',
+                    'fodeviceselection', 'profile',  'recording',"shortcuts",
+                    'livestreaming', 'etherpad', 'sharedvideo', 'settings', 'raisehand',
+                    'videoquality', 'filmstrip', 'stats', 'shortcuts',
+                    'tileview', 'videobackgroundblur', 'download', 'help','info'
+                ],
+                SETTINGS_SECTIONS: ['devices', 'language', 'moderator'],
+                SHOW_JITSI_WATERMARK: false,
+                SHOW_WATERMARK_FOR_GUESTS: false,
+                channelLastN: 1,
+                VERTICAL_FILMSTRIP: true,
+                SET_FILMSTRIP_ENABLED: false
+                
+            }
+            const options = {
+                roomName: streamCode,
+                interfaceConfigOverwrite : interfaceConfigLecturer,
+                userInfo : {
+                email : email
+                },
+                channelLastN: 1,
+            }
+            if(theStream.streamFrom !== email) await User.updateOne({email},{isStreaming : true})
+            return res.json({options : options, domain : domain, role : "Lecturer", name : name, isStreaming : true})
+        }else{ // Not-Owner
+            // For Stream Participant - *Not Class Owner*
+            const interfaceConfigStudent = {
+                TOOLBAR_BUTTONS: [
+                    'closedcaptions', 'fullscreen',
+                     'settings', 'raisehand',
+                ],
+                SETTINGS_SECTIONS: ['language'],
+                SHOW_JITSI_WATERMARK: false,
+                SHOW_WATERMARK_FOR_GUESTS: false,
+                VERTICAL_FILMSTRIP: false,
+                SET_FILMSTRIP_ENABLED: false
+                
+                // filmStripOnly: true
+            }
+            const optionsStudents = {
+                roomName: streamCode,
+                interfaceConfigOverwrite : interfaceConfigStudent,
+                userInfo : {
+                email : email
+                },
+                channelLastN: 1,
+        };
+            // Send Back Data Lah
+            return res.json({options : optionsStudents, domain : domain, role : "Student", name : name})
+        }
+        
+    }catch(err){
+        return res.json({err})
+    }
+
+})
+
+// Stop stream
+router.get("/stopStream", verify, async (req, res) => {
+    const owner = req.user.email
+    const _S = await Streaming.findOne({owner})
+    try{
+        // Find the stream and set the active state to false
+        const result = await Streaming.updateOne({ owner , isActive : true },{ isActive : false })
+        if (result.n >= 1){
+            // Set isStreaming state of User to false
+            const result2 = await User.updateOne({email:owner},{isStreaming : false})
+            if (result2.n >= 1){
+                return res.json({message : "Stop your current stream as successfully!", status : true})
+            }else{
+                return res.json({message : "Problem Occured during stop streaming process", status : false})
+            }           
+        }else{
+            return res.json({message : "Problem Occured during stop streaming process", status : false})
+        }
+
+    }catch(err){
+        return res.json(err)
+    }
+    //await axios.post('http://10.10.15.11:4000/deleteRoom',{roomId:_S.streamCode}).catch(err => console.log(err))
+})
+
+// Get currently stream of all class participated
+router.post("/getCurrentlyStream", verify , async (req, res) => {
+    var limit = req.body.limit == null ? 0 : req.body.limit
+    try{
+        const currentlyStreamings = await Streaming.find({isActive : true}).limit(limit).sort({date: -1});
+        return res.json(currentlyStreamings)
+    }catch(err){
+        return res.json(err)
+    }
+
+})
+
+// Get Stream Detials
+router.post("/getStreamDetail", verify , async (req, res) => {
+    const streamCode = req.body.streamCode
+    try{
+        const theStream = await Streaming.findOne({streamCode})
+        return res.json({
+            streamCode : theStream.streamCode,
+            streamTitle : theStream.streamTitle,
+            description : theStream.description,
+            ownerName : theStream.ownerName
+        })
+    }catch(err){
+        return res.json(err)
+    }
+
+})
 
 module.exports = router
